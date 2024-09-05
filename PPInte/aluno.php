78b85710fca90996c5197e1e335a487839faa603
RamonIFFAR <ramon.2022304839@aluno.iffar.edu.br>
2024-09-05 11:45:33 -0300

    correção dos arquivos por enquanto

--- PPInte/aluno.php
+++ PPInte/aluno.php
@@ -74,40 +74,39 @@
             Atualizar($_POST['id'], $_POST['matricula'], $_POST['telefone'], $_POST['email'], $_POST['nome'], $_POST['genero'], $_POST['cidade'], $_POST['dataNasc'], $_POST['moradia'], $_POST['cota'], $_POST['bolsa'], $_POST['orientador'], $_POST['reprovacao'], $_POST['equipTI']);
         }
         similar_text($UsoC->tipo, "DE", $percent);
-        if($percent  == 100) {
-            print "<form action='aluno.php' method='POST'>
-                        <input type='hidden' name='id' value=" . $aluno . "> <br>
+        if($percent  == 100) { ?>
+                    <form action='aluno.php' method='POST'>
+                        <input type='hidden' name='id' value="<?php echo $aluno ?>"> <br>
                         <label>Matrícula</label>
-                        <input type='text' name='matricula' value=". $aluno ."> <br>
+                        <input type='text' name='matricula' value="<?php echo $aluno ?>"> <br>
                         <label>Telefone</label>
-                        <input type='text' name='telefone' value=". $resSet['telefone'] ."> <br>
+                        <input type='text' name='telefone' value="<?php echo $resSet['telefone'] ?>"> <br>
                         <label>Email</label>
-                        <input type='email' name='email' value=". $resSet['email'] ."> <br>
+                        <input type='email' name='email' value="<?php echo $resSet['email'] ?>"> <br>
                         <label>Nome</label>
-                        <input type='text' name='nome' value=". $resSet['nome'] ."> <br>
+                        <input type='text' name='nome' value="<?php echo $resSet['nome'] ?>"> <br>
                         <label>Gênero</label>
-                        <input type='text' name='genero' value=". $resSet['genero'] ."> <br>
+                        <input type='text' name='genero' value="<?php echo $resSet['genero'] ?>"> <br>
                         <label>Cidade</label>
-                        <input type='text' name='cidade' value=". $resSet['cidade'] ."> <br>
+                        <input type='text' name='cidade' value="<?php echo $resSet['cidade']?>"> <br>
                         <label>Data de Nascimento</label>
-                        <input type='text' name='dataNasc' value=". $resSet['dataNasc'] ."> <br>
+                        <input type='text' name='dataNasc' value="<?php echo $resSet['dataNasc'] ?>"> <br>
                         <label>Moradia</label>
-                        <input type='text' name='moradia' value=". $resSet['moradia'] ."> <br>
+                        <input type='text' name='moradia' value="<?php echo $resSet['moradia'] ?>"> <br>
                         <label>Cota</label>
-                        <input type='text' name='cota' value=". $resSet['cota'] ."> <br>
+                        <input type='text' name='cota' value="<?php echo $resSet['cota'] ?>"> <br>
                         <label>Bolsa</label>
-                        <input type='text' name='bolsa' value=". $resSet['bolsa'] ."> <br>
+                        <input type='text' name='bolsa' value="<?php echo $resSet['bolsa'] ?>"> <br>
                         <label>Orientador</label>
-                        <input type='text' name='orientador' value=". $resSet['orientador'] ."> <br>
+                        <input type='text' name='orientador' value="<?php echo $resSet['orientador'] ?>"> <br>
                         <label>Reprovação</label>
-                        <input type='text' name='reprovacao' value=". $resSet['reprovacao'] ."> <br>
+                        <input type='text' name='reprovacao' value="<?php echo  $resSet['reprovacao'] ?>"> <br>
                         <label>Equipamento TI</label>
-                        <input type='text' name='equipTI' value=". $resSet['equipTI'] ."> <br>
+                        <input type='text' name='equipTI' value="<?php echo $resSet['equipTI'] ?>"> <br>
                         <button type='submit' name='atualizar'>Cadastrar</button>
                     </form>
-                    <button onclick=\"if(confirm('Tem certeza que deseja excluir esse aluno?')){location.href='aluno.php?id=" . $aluno ."&excluir=1';}else{false;} \">Excluir</button>"
-                    ;
-        } else {
+                    <button onclick="if(confirm('Tem certeza que deseja excluir esse aluno?')){location.href='aluno.php?id= <?php echo $aluno ?>&excluir=1'}">Excluir</button>
+        <?php } else {
                 print $resSet['matricula'] . "<br>";
                 print $resSet['telefone'] . "<br>";
                 print $resSet['email'] . "<br>";
@@ -123,14 +122,27 @@
                 print $resSet->equipTI . "<br>"; */
     }
 ?>
-<br> <span>Comentários</span>
+<br> <span>Comentários</span> <br>
+    <button onclick="location.href='aluno.php?id= <?php echo $aluno ?>&adicao=1'">Adicionar comentário</button>
     <?php
+    if(isset($_REQUEST['adicao'])){
+        ?>
+        <form action='add_comentario.php' method='POST'>
+            <input type='hidden' name='id_us' value='<?php echo $_SESSION['id_us'] ?>'>
+            <input type='hidden' name='id' value='<?php echo $aluno ?>'>
+            <label>Comentário</label> <br>
+            <input type='text' name='comentario'>
+            <button type='submit' name='comentar'>Adicionar comentário</button>
+        </form>
+
+        <?php
+    }
         while ($row = $resComentario->fetch_object()){
-            print "<br>" . $row->nome;
+            print "<br> <br>" . $row->nome . "<br>";
             print $row->descricao;
             if ($row->id_us == $_SESSION['id_us']){
-                print "<button onclick=\"location.href='ed_comentario.php?id_comentario=".$row->id_coment."'\">Editar comentário</button>";
-                print "<button onclick=\"location.href='add_comentario.php?remover=1&id_comentario=" . $row->id_coment . "'\">Remover Comentário</button>";
+                print "<br> <button onclick=\"location.href='ed_comentario.php?id_comentario=".$row->id_coment."'\">Editar comentário</button>";
+                print "<br> <button onclick=\"location.href='add_comentario.php?remover=1&id_comentario=" . $row->id_coment . "'\">Remover Comentário</button>";
             }
             }
     ?>
