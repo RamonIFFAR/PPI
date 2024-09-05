b2e4de7e8598a0c1ea84f682759b9445a57a2db9
RamonIFFAR <ramon.2022304839@aluno.iffar.edu.br>
2024-09-05 10:50:43 -0300

    correção do arquivo de edição de dados do profe

--- PPInte/professor.php
+++ PPInte/professor.php
@@ -20,57 +20,52 @@
     $UsoC = $ConsultaC->fetch_object();
     $qtdChecagem = $ConsultaC->num_rows;
 
-    function Atualizar($id, $cpf, $Siape, $email, $nome, $senha){
+    function Atualizar($id, $cpf, $Siape, $nome){
         include('config.php');
-        $sql = "UPDATE usuario SET cpf = '{$cpf}', Siape = '{$Siape}', email = '{$email}', nome = '{$nome}', senha = '{$senha}' where id_us = '{$id}'";
+        $sql = "UPDATE usuario SET cpf = '{$cpf}', Siape = '{$Siape}', nome = '{$nome}' where id_us = '{$id}'";
         $conn->query($sql) or die($conn->error);
         print "<script> location.href='professores.php'</script>";
     }
     // Variáveis para a conferência do usuário
     $sql = "select * from usuario where id_us = '{$prof}'";
     $res = $conn->query($sql);
-    $row = $res->fetch_object();
-    
+    $resSet = $res->fetch_object();
 
     if ($qtdChecagem > 0) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SGE</title>
+    <title>SGAE</title>
 </head>
 <body>
-    <h1>Edite as informações desejadas</h1>
-    <span>Deixe as informações como estão se não deseja alterá-la</span> <br>
     <?php 
         if(isset($_POST['atualiza'])){
-            Atualizar($_POST['id_prof'], $_POST['cpf'], $_POST['siape'], $_POST['email'], $_POST['nome'], $_POST['senha']);
+            Atualizar($_POST['id_prof'], $_POST['cpf'], $_POST['siape'], $_POST['nome']);
         }
         similar_text($UsoC->tipo, "DE", $percent);
-        if($percent  == 100) {
-            print "<form action='professor.php' method='POST'>
-                        <input type='hidden' name='id_prof' value=" . $prof . "> <br>
+        if($percent  == 100) { ?>
+                    <form action='professor.php' method='POST'>
+                        <h1>Edite as informações desejadas</h1>
+                        <span>Deixe as informações como estão se não deseja alterá-la</span> <br>
+                        <input type='hidden' name='id_prof' value="<?php echo $prof ?>"> <br>
                         <label>CPF</label>
-                        <input type='text' name='cpf' value=" . $row->cpf ."> <br>
+                        <input type='text' name='cpf' value=" <?php echo $resSet->cpf ?>"> <br>
                         <label>Siape</label>
-                        <input type='text' name='siape' value=" . $row->Siape . "> <br>
-                        <label>Email</label>
-                        <input type='text' name='email' value=" . $row->email ."> <br>
+                        <input type='text' name='siape' value=" <?php echo $resSet->Siape ?> "> <br>
                         <label>Nome</label>
-                        <input type='text' name='nome' value=" . $row->nome . "> <br>
-                        <label>Senha</label>
-                        <input type='text' name='senha' value=" . $row->senha . "> <br>
+                        <input type='text' name='nome' value=" <?php echo $resSet->nome ?>"> <br>
                         <button type=submit name='atualiza'>Salvar alterações</button>
                     </form> <br>
-                    <button onclick=\"if(confirm('Tem certeza que deseja excluir esse perfil de professor?')){location.href='professor.php?id_prof=" . $prof ."&excluir=1';}else{false;} \">Excluir</button>";
-        } else {
-            while($row = $res->fetch_object()){
-                print "<br>" . $row->nome;
-                
-            }
+                    <button onclick="if(confirm('Tem certeza que deseja excluir esse perfil de professor?')){location.href='professor.php?id_prof= <?php echo $prof  ?>&excluir=1'}">Excluir</button>
+        <?php } else {
+                print $resSet->nome . "<br>";
+                print $resSet->Siape . "<br>";
+                print $resSet->email . "<br>";
+                print $resSet->cpf . "<br>";
     }
     } else {
         print "<script>location.href='index.php'</script>";
