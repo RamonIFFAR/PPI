74068d48b943576fd26bd284beff401ef98651bf
RamonIFFAR <ramon.2022304839@aluno.iffar.edu.br>
2024-09-08 23:46:01 -0300

    Banco atualizado
    Versão do banco de dados atualizada do dia 08/09/2024

--- abcd.sql
+++ abcd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 22/08/2024 às 15:55
+-- Tempo de geração: 09/09/2024 às 04:43
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.0.30
 
@@ -43,6 +43,14 @@ CREATE TABLE `aluno` (
   `equipTI` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Despejando dados para a tabela `aluno`
+--
+
+INSERT INTO `aluno` (`matricula`, `telefone`, `email`, `nome`, `genero`, `cidade`, `dataNasc`, `moradia`, `cota`, `bolsa`, `orientador`, `reprovacao`, `equipTI`) VALUES
+(1, '123', 'teste@gmail.com', 'teste de novo', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste'),
+(333, 'teste', 'teste@gmail.com', 'teste', 'teste', 'teste', 'teste', 'teste', 'teste', 'q?', 'teste', 'teste', 'teste');
+
 -- --------------------------------------------------------
 
 --
@@ -56,6 +64,36 @@ CREATE TABLE `comentario` (
   `descricao` varchar(450) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Despejando dados para a tabela `comentario`
+--
+
+INSERT INTO `comentario` (`id_coment`, `matricula`, `id_us`, `descricao`) VALUES
+(23, 333, 28, 'mais um testezinho');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `curso`
+--
+
+CREATE TABLE `curso` (
+  `id_curso` int(11) NOT NULL,
+  `duracao` varchar(45) DEFAULT NULL,
+  `descricao` varchar(200) DEFAULT NULL,
+  `nome` varchar(45) DEFAULT NULL,
+  `foto` varchar(455) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `curso`
+--
+
+INSERT INTO `curso` (`id_curso`, `duracao`, `descricao`, `nome`, `foto`) VALUES
+(2, '100 horas', 'É um curso bem legal assim que eu gosto bastante', 'Técnico em testação', 'fotos/08-09-2024-Mapa inicial.jpeg'),
+(4, '200 horas', 'ajudar as pessoas', 'Técnico em gentileza', ''),
+(5, 'instantânea', 'É um curso bem legal assim que eu gosto bastante', 'Joao do teste', 'fotos/08-09-2024-8asm99.jpg');
+
 -- --------------------------------------------------------
 
 --
@@ -63,15 +101,20 @@ CREATE TABLE `comentario` (
 --
 
 CREATE TABLE `professor` (
-  `id_prof` int(11) NOT NULL
+  `id_prof` int(11) NOT NULL,
+  `foto` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `professor`
 --
 
-INSERT INTO `professor` (`id_prof`) VALUES
-(31);
+INSERT INTO `professor` (`id_prof`, `foto`) VALUES
+(33, NULL),
+(34, NULL),
+(35, NULL),
+(36, NULL),
+(37, NULL);
 
 -- --------------------------------------------------------
 
@@ -121,7 +164,12 @@ INSERT INTO `usuario` (`cpf`, `Siape`, `foto`, `id_us`, `email`, `nome`, `senha`
 ('admin', 'nenhum', NULL, 28, 'admin@gmail.com', 'admin', 'admin'),
 ('888888', 'nulo', NULL, 29, 'nulo@gmail.com', 'nulo', 'nulo'),
 ('777777777', 'podre', NULL, 30, 'junior@gmail.com', 'ligma', '123'),
-('dummy', 'dummy', NULL, 31, 'dummy@gmail.com', 'Tonhão', 'dummy');
+('036.114.170', '1325467', NULL, 32, 'GalPodre@gmail.com', 'Joao dos games', '1234'),
+('NUNCA', '1325467', NULL, 33, 'ramonzcezaro@gmail.com', 'teste top', 'legales'),
+(' NUNCA', ' 1325467 ', 'fotos/08-09-2024-Senhor veríssimo Palmitinho.jpg', 34, 'GalPodre@gmail.com', ' Tonhão', 'Maconharia'),
+('nada', 'nada', 'fotos/08-09-2024-9c22544c153227b66ac105d6821203c9.jpg', 35, 'nada@gmail.com', 'nada', 'nada'),
+('036.114.170', '99', 'fotos/08-09-2024-8asm99.jpg', 36, 'theplaguerofc@gmail.com', 'Ramon', 'Maconharia'),
+('sigmaligma', 'teste', 'fotos/08-09-2024-ramsesii.png', 37, 'Thales@gmail.com.br', 'teste', 'pou');
 
 --
 -- Índices para tabelas despejadas
@@ -140,6 +188,12 @@ ALTER TABLE `comentario`
   ADD PRIMARY KEY (`id_coment`),
   ADD KEY `id_us` (`id_us`),
   ADD KEY `matricula` (`matricula`);
+
+--
+-- Índices de tabela `curso`
+--
+ALTER TABLE `curso`
+  ADD PRIMARY KEY (`id_curso`);
 
 --
 -- Índices de tabela `professor`
@@ -167,13 +221,19 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de tabela `comentario`
 --
 ALTER TABLE `comentario`
-  MODIFY `id_coment` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_coment` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT de tabela `curso`
+--
+ALTER TABLE `curso`
+  MODIFY `id_curso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_us` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id_us` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- Restrições para tabelas despejadas
