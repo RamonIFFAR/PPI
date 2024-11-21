5796897ade6cdd51d9f494fa0ebf291cd147f88a
RamonIFFAR <ramon.2022304839@aluno.iffar.edu.br>
2024-11-21 19:48:57 -0300

    Update turmas.php
    update linha 56 até 68

--- PPInte/turmas.php
+++ PPInte/turmas.php
@@ -17,6 +17,18 @@
     $tes = $QChecagem->fetch_object();
     $procura = $QChecagem->num_rows; 
 
+    // Usado para conferir se o setor é da DE
+    $Checagem = "select * from setor where id_set = '{$_SESSION['id_us']}' and tipo like 'DE'";
+    $ConsultaC = $conn->query($Checagem);
+    $UsoC = $ConsultaC->fetch_object();
+    $qtdChecagem = $ConsultaC->num_rows;
+
+    // Confere se o usuário é um professor
+    $Cprof = "select * from professor where id_prof = '{$_SESSION['id_us']}'";
+    $resProf = $conn->query($Cprof);
+    $rowProf = $resProf->fetch_object();
+    $qtdProf = $resProf->num_rows;
+
     // XXXXXXXXXX If que confere se o usuário está logado XXXXXXXXXXXXXX
     if($procura > 0){
 
@@ -25,22 +37,34 @@
         print"<script>location.href=index.php</script>";
     }
 
-    $sql = "select nome, id from turma";
-    $res = $conn->query($sql);
+    $sqlTurmaADM = "select * from turma";
+    $res = $conn->query($sqlTurmaADM) or die($conn->error);
 
+    $sqlTurmaProf = "select id, nome from turma where id = any (select id_turma from professor_turma where id_prof = '{$_SESSION['id_us']}')";
+    $resTurmaProf = $conn->query($sqlTurmaProf);
+    
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SGAE: Turmas</title>
 </head>
 <body>
+    <a href="painel.php">Voltar para o início</a><br> <br>
     <?php 
-        while($row = $res->fetch_object()){
-            echo "Turma ".$row->nome."<a href='turma.php?id=". $row->id ."'> Informações</a><br>";
+        //  XXXXXXXXXX If que confere se o usuário é um setor DE XXXXXXXXXXXXXX
+        if($qtdChecagem > 0){
+            while($row = $res->fetch_object()){
+                echo "Turma ".$row->nome."<a href='turma.php?id=". $row->id ."'> Informações</a><br>";
+            }
+        } else if($qtdProf > 0){
+            while($rowTurmaProf = $resTurmaProf->fetch_object()){
+                echo "Turma".$rowTurmaProf->nome." <a href='turma.php?id=". $rowTurmaProf->id ."'> Informações</a><br>";
+            }
         }
+        
     ?>
 </body>
 </html>
